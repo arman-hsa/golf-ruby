e4c48247b949c57e77fd303771be3fce1e3e8914
arman-hsa <12447589+arman-hsa@users.noreply.github.com>
2018-08-27 16:07:16 -0600

    Hole_1.rb

---
+++ HOLE_1.rb
@@ -0,0 +1,13 @@
+# Create a method that takes in an array, sums the array, then returns the array and the sum.
+
+def array
+  a = [1, 2, 3, 4, 5]
+  puts a
+  sum = 0
+  a.each { |a| sum += a }
+  puts sum
+end
+
+
+
+array
